fd13a96859de47cb72679bd8adf8d58f6a4eb9c3
brijes-h <brijesh_1210@yahoo.com>
2021-11-11 12:38:37 +0530

    settings.py file for constants

---
+++ settings.py
@@ -0,0 +1,14 @@
+# Constants
+
+WIDTH = 500
+HEIGHT = 500
+ROWS = 3
+COLS = 3
+SQUARE_SIZE = 166
+LINE_WIDTH = 7
+BG_COLOR = (171,219,227)
+C_RADIUS = 50
+C_WIDTH = 12
+LINE_COLOR = (21,76,121) # rgb values
+CROSS_WIDTH = 20
+SPACE = 37  # space between the line and cross

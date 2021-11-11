67931980223ae0613951e0bd64c03cd56db06ed7
brijes-h <brijesh_1210@yahoo.com>
2021-11-11 18:58:05 +0530

    update constants

--- settings.py
+++ settings.py
@@ -1,14 +1,21 @@
 # Constants
 
+# dimensions
 WIDTH = 500
 HEIGHT = 500
 ROWS = 3
 COLS = 3
 SQUARE_SIZE = 166
 LINE_WIDTH = 7
-BG_COLOR = (171,219,227)
 C_RADIUS = 50
 C_WIDTH = 12
-LINE_COLOR = (21,76,121) # rgb values
 CROSS_WIDTH = 20
 SPACE = 37  # space between the line and cross
+
+# colors -> rgb values
+BG_COLOR = (171,219,227)
+LINE_COLOR = (21,76,121) 
+O_COLOR = (237, 233, 213)
+X_COLOR = (55,49,39,255)
+
+
